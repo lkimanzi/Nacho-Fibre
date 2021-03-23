7264e5e7af4d664bd7168221d9f56df84cb39128
Lucas Kimanzi <lkimanzi@hotmail.com>
2021-03-23 19:20:38 -0400

    Pseudo code

--- NachoFibre.js
+++ NachoFibre.js
@@ -1,9 +1,13 @@
 
 class NachoFibre{
 
-    fibonacci(n){
-        return -1;
-    }
+//if the given num is 0 then return sequence as 1 
+//else if it is 1 then return the second number
+//If the num is greater than 2 then;
+//iterate till num -the given number 
+//add num previous  number + (num -2)
+//return iteration value for num
+
 
     kaleb(n){
         return -1;
